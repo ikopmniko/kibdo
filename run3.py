4102adba8c109fc484512354fe8982640fc44268
ikopmniko <hannapitman786@gmail.com>
2025-04-13 13:20:15 +0700

    Update run3.py

--- run3.py
+++ run3.py
@@ -22,11 +22,11 @@ def read_csv_range(filename, start, end):
     return rows
 
 # Rentang data yang diproses (misal dari baris 1 sampai 50)
-start_row =6000  # Baris pertama (0-based index)
-end_row = 6200  # Baris terakhir yang ingin diproses
+start_row =6200  # Baris pertama (0-based index)
+end_row = 6400  # Baris terakhir yang ingin diproses
 
 # Deklarasi akun tunggal
-email = "saveyumckinlc"
+email = "KirandaZafari"
 password = "@@Eskepal123"
 
 # Baca judul video sesuai rentang yang diinginkan
